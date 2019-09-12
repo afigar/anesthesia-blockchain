f8a03cc907e6219caafaddd9ef73e3405252dcbc
Alejandro Figar Gutierrez <afigar@gmail.com>
2019-09-12 14:45:47 -0300

    IntellitoSNS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # anesthesia-blockchain
-Nodejs code for a REST API to save data to Bigchaindb test site.
+Nodejs code for a AWS Lambda function which exposes a REST API that sends data to AWS SNS service. Each SNS message triggers another AWS Lambda function that saves each message to Bigchaindb test site.
--- handler.js
+++ handler.js
@@ -1,15 +1,10 @@
 'use strict';
-//const driver = require('bigchaindb-driver');
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const AWS = require('aws-sdk');
 const bodyParser = require('body-parser');
 AWS.config.update({region: 'us-east-1'});
-
-//const alice = new driver.Ed25519Keypair();
-//const conn = new driver.Connection('http://test-bigchaindb.for-our.info:9984/api/v1/');
-
 
 function addslashes(string) {
   return string.replace(/\\/g, '\\\\').
@@ -33,6 +28,7 @@ app.get('/help/', (req, res) => {
     res.send(req.body) ;
     res.status(200).end();
 });
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
@@ -46,13 +42,10 @@ app.post('/post/data', function(req, res) {
 });
 
 app.post('/api/v1/', async (req, res) => {
-//app.use(function (req, res, next) {
-  //console.log(req.body) // populated!
   const auxmessage = { 'message': req.body }
   console.log(auxmessage)
-  //res.send(addslashes(JSON.stringify(req.body)));
   var params = {
-    Message: addslashes(JSON.stringify(req.body)), /* required */
+    Message: addslashes(JSON.stringify(req.body)), 
     TopicArn: 'arn:aws:sns:us-east-1:212507307353:IntelliChain'
   };
   var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
@@ -64,41 +57,8 @@ app.post('/api/v1/', async (req, res) => {
       function(err) {
       console.error(err, err.stack);
     });
-  /*const tx = driver.Transaction.makeCreateTransaction(
-    {
-      datetime: new Date().toString(),
-      location: 'Maternity',
-      or: 'q11',
-      device: {
-          model: 'Philips Intellivue MP40',
-          id: '1234asbdfsg',
-      }
-    }  ,
-    { datetime: new Date().toString(),
-      location: 'Maternity',
-      or: 'q11',
-      device: {
-          model: 'Philips Intellivue MP40',
-          id: '1234asbdfsg',
-      },
-      message: addslashes(JSON.stringify(req.body))
-    },
-    [ driver.Transaction.makeOutput(
-        driver.Transaction.makeEd25519Condition(alice.publicKey))],
-    alice.publicKey)
-      const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
-      //console.log(txSigned)
-      await conn.postTransactionCommit(txSigned).then(function(retrievedTx){
-        //console.log('Transaction', retrievedTx.id, 'successfully posted.')
-        //res.send( 'Transaction', retrievedTx.id, 'successfully posted.'	)
-      },function(err){
-        console.log(err)
-      }); */
-      //context.done(null, "Todo salio bien");
-      res.send("Todo bien: ")
+      res.send("Success!")
       res.status(200).end();
-      //next()
 });
 
-//app.listen(3000);
 module.exports.generic = serverless(app);
